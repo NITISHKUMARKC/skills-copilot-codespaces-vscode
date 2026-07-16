c1007f7a56a485b2eb7a51d640fd9a82f0447ce5
NITISH KUMAR KC <kcnitishkumar39@gmail.com>
2026-07-16 16:44:12 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+  const member = {
+    name: "NITISH KUMAR KC",
+    role: "Computer Engineering Student",
+    skills: ["JavaScript", "GitHub Copilot", "Git"]
+  };
+
+  return member;
+}
